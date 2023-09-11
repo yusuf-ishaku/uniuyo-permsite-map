2c665d549393e12ab59c8313f7cefa47c6b795c6
Yusuf-ishaku <kingshakzy@gmail.com>
2023-09-11 17:42:07 +0200

    added map bounds

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,17 @@
   onMounted(()=>{
     mapboxgl.accessToken = `pk.eyJ1Ijoic2hha3p5IiwiYSI6ImNsbHh3bmpkNDJrN3czcHA4eWlsbGo5cXgifQ.dI63IIgQ30AaQrmQLwBsaA`;
     const nav = new mapboxgl.NavigationControl();
-    const map =new mapboxgl.Map(
+    const bounds = [
+        [7.972914, 5.028030],
+       [7.981987, 5.047798]
+    ];
+    const map = new mapboxgl.Map(
       {
         container: "mapcontainer",
         style: `mapbox://styles/shakzy/cllzhauhc00o501pba4gt0zhi`,
-        center: [5.037636, 7.977964],
-        zoom: 12,
+        center: [ 7.977964,5.037636],
+        zoom: 1,
+        maxBounds: bounds
       }
     );
    map.addControl(nav, "top-right");
