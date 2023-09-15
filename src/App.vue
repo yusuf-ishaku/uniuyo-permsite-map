0084eb49ad217e2d96578a0bc78ca90d4b6b4de7
yusuf <kingshakzy@gmail.com>
2023-09-16 00:12:08 +0100

    Add refspec

--- src/App.vue
+++ src/App.vue
@@ -47,14 +47,14 @@ function handleShow(t){
 </script>
 
 <template>
-  <div class="w-[100vw] h-[100vh] overflow-hidden fixed flex flex-col items-center ">
+  <div class="w-[100vw] h-[100vh] overflow-hidden flex flex-col items-center ">
     <div class="w-[100vw] h-[100vh] flex flex-col items-center border-t-[1px]">
       <div class="w-[100vw] h-[90vh]">
         <HomeViewVue>
 
         </HomeViewVue>
       </div>
-      <nav class="w-full z-50 bg-white top-[90%] h-[10vh] flex flex-row items-center justify-between font-normal px-10 py-2 sm:px-20" >
+      <nav class="w-full z-50 bg-white fixed bottom-0  h-[10vh] flex flex-row items-center justify-between font-normal px-10 py-2 sm:px-20" >
         <button @click="handleShow('search')" class="font-poppins flex flex-col items-center" >
           <span class="material-symbols-outlined">
             search
